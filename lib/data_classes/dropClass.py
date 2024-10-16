cf2627f03c0713245b6da7d35b3e19b299252a1e
Stephen Adusei <st.adusei@ufl.edu>
2024-10-16 00:38:14 -0400

    yes

--- lib/data_classes/dropClass.py
+++ lib/data_classes/dropClass.py
@@ -698,7 +698,7 @@ class Drop:
             
             # Flip the sign of displacement column and make it zero at the start
             self.impulse_df[col_names[2]] = -1 * (self.impulse_df[col_names[2]] - self.impulse_df[col_names[2]].iloc[0])
-            self.impulse_df[0, col_names[2]].iloc[0] = -1 * self.impulse_df[0, col_names[2]].iloc[0]
+            self.impulse_df[0, col_names[2]].iloc[0] = 0.0
 
         # Update the units
         self.units["accel"] = "m/s^2"
